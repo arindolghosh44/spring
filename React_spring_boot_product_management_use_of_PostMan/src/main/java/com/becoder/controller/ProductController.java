1ac5a82385c6676467f1b5591eefe4e0febfe543
arindolghosh44 <arindolghosh.44@gmail.com>
2024-03-18 07:14:39 +0530

    react

--- React_spring_boot_product_management_use_of_PostMan/src/main/java/com/becoder/controller/ProductController.java
+++ React_spring_boot_product_management_use_of_PostMan/src/main/java/com/becoder/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.becoder.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.becoder.model.Product;
 import com.becoder.service.ProductService;
-
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 public class ProductController {
 
