0a5e0d7be6be81ab875f65cd3ff5688c34507a35
Arindol Ghosh <144908811+arindolghosh44@users.noreply.github.com>
2024-10-29 20:38:46 +0530

    fghfhfhf

---
+++ Urban_cart_ecommerce/src/main/java/com/aec/controller/AdminController.java
@@ -0,0 +1,45 @@
+package com.aec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	
+	@GetMapping("/")
+	public String index() {
+		
+		
+		return "admin/index";
+		
+	}
+	
+	
+	@GetMapping("/loadAddProduct")
+	public String loadAddProduct() {
+		
+		
+		return "admin/loadAddProduct";
+		
+	}
+	
+	
+	
+	
+	@GetMapping("/Category")
+	public String Category() {
+		
+		
+		return "admin/Category";
+		
+	}
+	
+	
+	
+	
+	
+
+}
